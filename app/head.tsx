96f05a7480aff5c96143bf8c56d59e40075f1c2a
Aikawa <cesar.lojan8@gmail.com>
2023-04-18 22:40:13 +0000

    posts

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+const head = () => {
+    return (
+        <>
+            <title></title>
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            
+        </>
+    )
+}
+
+export default head;
